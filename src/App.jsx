ea7a32d6bf4fd6010056cdbc7d1c4e08241729d1
Vivek-68 <vivektitan3@gmail.com>
2023-07-21 01:06:17 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState , useEffect} from "react"
+import axios from 'axios'
+
+function App() {
+  
+const [coins,setCoins] = useState([])
+const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en'
+
+useEffect(()=>{
+  axios.get(url).then((res) => console.log(res.data))
+  .catch(err => console.log(err))
+},[])
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
