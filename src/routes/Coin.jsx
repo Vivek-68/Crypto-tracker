a5cd147bcf2ad69f49c8b0144b74f99ee0594456
Vivek-68 <vivektitan3@gmail.com>
2023-07-22 02:26:29 +0530

    fixed responsiveness

--- src/routes/Coin.jsx
+++ src/routes/Coin.jsx
@@ -9,6 +9,8 @@ const Coin = () => {
   const params = useParams()
   const [coin,setCoin] = useState({})
   const url = `https://api.coingecko.com/api/v3/coins/${params.coinId}`
+
+  
   useEffect(()=>{
     axios.get(url).then((response)=>{
       setCoin(response.data).catch((err)=>{
